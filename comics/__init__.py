afee0a950e155ab39ee9ae4ca46b749f2e007760
siro0230 <xminh369@gmail.com>
2026-01-15 23:05:09 +0700

    First commit: Init

---
+++ comics/__init__.py
@@ -0,0 +1,15 @@
+"""
+KumoComic - A HakuNeko-inspired manga/comic downloader
+
+This library provides a connector-based architecture for downloading
+manga and comics from various websites.
+
+:copyright: (c) 2026 0xSi23
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "comics"
+__author__ = "0xSi23"
+__license__ = "MIT"
+__copyright__ = "2026 0xSi23"
+__version__ = "0.1.0"
